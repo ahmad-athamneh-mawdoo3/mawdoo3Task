2f13588f87985e11d1c46592cd643540570ea406
ahmad athamneh <ahmad.athamneh@optimumpartners.co>
2018-11-17 22:30:19 +0200

    fix error

--- src/Views/saved_results.blade.php
+++ src/Views/saved_results.blade.php
@@ -1,7 +1,7 @@
 @extends('task::app') 
 @section('content')
 <div class="container">
-
+@if(count($data)>0)
     <table border="1"  class="table table-hover">
         <thead>
             <th>title</th>
@@ -11,24 +11,25 @@
         </thead>
         <tbody>
             @foreach($data as $k=>$item)
-            <tr>
-                <td>{{$item["title"] }}</td>
-                <td> {{ $item["description"] }}</td>
-                <td>{{ $item["link"] }}</td>
-                <td>
-                    <form method="post" class="form-horizontal" name="form{{$item['id']}}" action="{{ route('ActionResult',$item['id']) }}" >
-                        <textarea  id="comment" name="comment" value="{{$item['comment']}}" data-old="" >{{$item['comment']}}</textarea>
-                        <input type="hidden" id="delId" name="delId" value="{{$item['id']}}" data-old="{{$item['title']}}" >
-                        @csrf
-                        <button type="submit" name="action"  value="Delete" class="btn btn-primary">{{ __("Delete") }}</button>
-                        <button type="submit" name="action"  value="Update" class="btn btn-primary">{{ __("Update") }}</button>
-                    </form>
-                </td>
-            </tr>
+                <tr>
+                    <td>{{$item["title"] }}</td>
+                    <td> {{ $item["description"] }}</td>
+                    <td>{{ $item["link"] }}</td>
+                    <td>
+                        <form method="post" class="form-horizontal" name="form{{$item['id']}}" action="{{ route('ActionResult',$item['id']) }}" >
+                            <textarea  id="comment" name="comment" value="{{$item['comment']}}" data-old="" >{{$item['comment']}}</textarea>
+                            <input type="hidden" id="delId" name="delId" value="{{$item['id']}}" data-old="{{$item['title']}}" >
+                            @csrf
+                            <button type="submit" name="action"  value="Delete" class="btn btn-primary">{{ __("Delete") }}</button>
+                            <button type="submit" name="action"  value="Update" class="btn btn-primary">{{ __("Update") }}</button>
+                        </form>
+                    </td>
+                </tr>
             @endforeach
 
         </tbody>
     </table>
+@endif
 
  
 </div>
